9f34e5890418188f29af755113b8da64dc1cf323
Brad Carter <bcart3r@gmail.com>
2012-06-19 01:08:44 -0400

    move read and write handler to irc

--- irc/irc.go
+++ irc/irc.go
@@ -3,13 +3,14 @@ package irc
 import (
 	"net"
 	"bufio"
+	"fmt"
 )
 
 type Irc struct {
-	R     *bufio.Reader
-	W     *bufio.Writer
-	Read  chan string
-	Write chan string
+	Reader *bufio.Reader
+	Writer *bufio.Writer
+	R      chan string
+	W      chan string
 }
 
 func Connect(server string) *Irc {
@@ -24,4 +25,30 @@ func Connect(server string) *Irc {
 	out := make(chan string, 1000)
 
 	return &Irc{reader, writer, in, out}
+}
+
+func (i *Irc) ReadHandler() {
+	go func() {
+		for {
+			ln, err := i.Reader.ReadString(byte('\n'))
+			if err != nil {
+				fmt.Println(err)
+			}
+			i.R <- ln
+		}
+	}()
+}
+
+func (i *Irc) WriteHandler() {
+	go func() {
+		for {
+			str := <-i.W
+			_, err := i.Writer.WriteString(str + "\r\n")
+			if err != nil {
+				fmt.Println(err)
+			}
+
+			i.Writer.Flush()
+		}
+	}()
 }
