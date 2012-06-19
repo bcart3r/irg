465cf7f2a30a2408bcfd095a2a9689bc0d0fa25c
Brad Carter <bcart3r@gmail.com>
2012-06-19 01:09:27 -0400

    add helper methods

--- irg.go
+++ irg.go
@@ -2,7 +2,8 @@ package irg
 
 import (
 	"fmt"
-	"./irc"
+	"irg/irc"
+	"regexp"
 )
 
 type Bot struct {
@@ -11,28 +12,33 @@ type Bot struct {
 	Conn       *irc.Irc
 }
 
-func (b *Bot) readHandler() {
-	go func() {
-		for {
-			ln, err := b.Conn.R.ReadString(byte('\n'))
-			if err != nil {
-				fmt.Println(err)
-			}
-			b.Conn.Read <- ln
-		}
-	}()
+func (b *Bot) Login(nick, name string) {
+	b.setNickName(nick, name)
+	b.Conn.W <- "NICK " + b.Nick
+	b.Conn.W <- "NAME " + b.Nick + " 0 * :" + b.Name
 }
 
-func (b *Bot) writeHandler() {
-	go func() {
-		for {
-			str := <-b.Conn.Write
-			_, err := b.Conn.W.WriteString(str + "\r\n")
-			if err != nil {
-				fmt.Println(err)
-			}
+func (b *Bot) setNickName(nick, name string) {
+	b.Nick = nick
+	b.Name = name
+}
 
-			b.Conn.W.Flush()
+func Connect(server string) *Bot {
+	conn := irc.Connect(server)
+	events := make(chan string, 1000)
+
+	conn.ReadHandler()
+	conn.WriteHandler()
+
+	return &Bot{"GoBot", "GoBot", events, conn}
+}
+
+func (b *Bot) RunLoop() {
+	for {
+		ln := <-b.Conn.R
+		fmt.Print(ln)
+		if regexp.MustCompile("^PING").Match([]byte(ln)) {
+			b.Conn.W <- "PONG " + regexp.MustCompile(":.*").FindString(ln)
 		}
-	}()
+	}
 }
